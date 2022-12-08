a2ad907db968e34372ea2851459eee870ea50788
Pranjal <pranjalvds91@gmail.com>
2022-12-09 00:40:13 +0530

    add read me file and other related files

---
+++ README.txt
@@ -0,0 +1,7 @@
+Please install below softaware and do path setup
+jdkversion = 1.8.0_271
+jreversion = 1.8.0_271
+maven version = 3.2.5
+Go to project root folder and 
+commandToRun 
+mvn test
--- src/test/java/CucumberRunner.java
+++ src/test/java/CucumberRunner.java
@@ -5,15 +5,19 @@ import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-		features = "src\\test\\resources\\features\\SeleniumIpriceScenarios.feature"
+		features = "src\\test\\resources\\features"
 		,glue={"stepDefinition"},
+		       strict = true,
+				
 				plugin = { "pretty",
                         "json:target/cucumber-report/cucumber.json",
                         "html:target/cucumber-report/cucumber.html",
                         "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"
                         
 		},
+						
 				          monochrome = true
+				          
 		)
 
 ///SeleniumAutomation/src/test/resources/features/SeleniumIpriceScenarios.feature
